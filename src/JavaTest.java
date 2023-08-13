b34a6564e2cee5c685d221a4e6f4834b1390a6dd
unknown <srilalitha1015@gmail.com>
2023-08-13 19:56:40 +0530

    commit changes

--- src/JavaTest.java
+++ src/JavaTest.java
@@ -1,10 +1,9 @@
-
 public class JavaTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Test.....");
+		System.out.println("Test Welcomeee.......");
 	}
 
 }
